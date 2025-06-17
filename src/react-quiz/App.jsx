3732b0b5e61daca0deb36f4b1b22c11627274a19
Paula <paulaoorjuela@gmail.com>
2025-06-17 12:57:36 -0500

    handle answer selection and points calculation

--- src/react-quiz/App.jsx
+++ src/react-quiz/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import Loader from "./components/Loader"
 import Error from "./components/Error"
 import Question from "./components/Question"
-
 import "./index.css";
 import Main from "./Main";
 import StartScreen from "./components/StartScreen";
@@ -11,20 +10,27 @@ import StartScreen from "./components/StartScreen";
 const InitialState = {
     questions:[],
     status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
-    index: 0
+    index: 0,
+    answer: null,
+    points: 0
 }
 function reducer(state, action){
     switch(action.type){
         case 'dataRecived' : return { ...state, questions: action.payload, status: 'ready'}
         case 'dataError' : return{ ...state, status: 'error'}
         case 'startQuiz' : return { ...state, status: 'active'}
+        case 'newAnswer' : 
+            const currentQuestion = state.questions.at(state.index)
+            return { ...state, answer: action.payload, points: action.payload === currentQuestion.correctOption ? 
+                state.points + currentQuestion.points : state.points
+            }
         default: throw new Error("Unknown action type"); 
     }
 
 }
 
 export default function App() {
-    const [{ questions, status, index }, dispatch] = useReducer(reducer, InitialState)
+    const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, InitialState)
 
     const numQuestions = questions.length;
 
@@ -43,7 +49,7 @@ export default function App() {
                 {status === 'loading' && <Loader />}
                 {status === 'error' && <Error />}
                 {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-                {status === 'active' && <Question question={questions[index]}/>}
+                {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer}/>}
             </Main>
         </div>
     );
