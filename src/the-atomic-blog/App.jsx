85a8cf114c3259aedb6b1567658114471d231583
Paula <paulaoorjuela@gmail.com>
2025-07-02 19:18:21 -0500

    add custom hook usePosts

--- src/the-atomic-blog/App.jsx
+++ src/the-atomic-blog/App.jsx
@@ -1,6 +1,7 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import "./style.css";
+import { PostProvider, usePosts } from "./PostProvider";
 
 function createRandomPost() {
   return {
@@ -9,34 +10,11 @@ function createRandomPost() {
   };
 }
 
-// Create a new context
-const PostContext = createContext();
+
 
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
-  const [searchQuery, setSearchQuery] = useState("");
-  const [isFakeDark, setIsFakeDark] = useState(false);
-
-  // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
-      : posts;
-
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-  }
-
-  function handleClearPosts() {
-    setPosts([]);
-  }
-
+    const [isFakeDark, setIsFakeDark] = useState(false);
+  
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
@@ -46,36 +24,26 @@ function App() {
   );
 
   return (
-    // Provide value to children components
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      <section>
-        <button
-          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-          className="btn-fake-dark-mode"
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
         >
-          {isFakeDark ? "☀️" : "🌙"}
-        </button>
-
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+      <PostProvider>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </section>
-    </PostContext.Provider>
+      </PostProvider>
+    </section>
   );
 }
 
 function Header() {
   // consuming the context value
-  const { onClearPosts } = useContext(PostContext)
+  const { onClearPosts } = usePosts()
   return (
     <header>
       <h1>
@@ -91,7 +59,7 @@ function Header() {
 }
 
 function SearchPosts() {
-  const { searchQuery, setSearchQuery } = useContext(PostContext)
+  const { searchQuery, setSearchQuery } = usePosts()
   return (
     <input
       value={searchQuery}
@@ -102,7 +70,7 @@ function SearchPosts() {
 }
 
 function Results() {
-  const { posts } = useContext(PostContext)
+  const { posts } = usePosts()
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
@@ -124,7 +92,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-  const { onAddPost } = useContext(PostContext)
+  const { onAddPost } = usePosts()
 
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -155,7 +123,7 @@ function FormAddPost() {
 }
 
 function List() {
-  const { posts } = useContext(PostContext)
+  const { posts } = usePosts()
   return (
     <ul>
       {posts.map((post, i) => (
@@ -169,7 +137,7 @@ function List() {
 }
 
 function Archive() {
-  const { onAddPost } = useContext(PostContext)
+  const { onAddPost } = usePosts()
   // Here we don't need the setter function. We're only using state to store these posts
   // because the callback function passed into useState (which generates the posts)
   // is only called once, on the initial render. So we use this trick as an optimization technique,
