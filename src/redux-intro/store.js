7b3ff9eba28bb5e0d924f80bd2d50aeb914e7bdb
Paula <paulaoorjuela@gmail.com>
2025-08-13 12:40:00 -0500

    update: redux-intro, add action creators

--- src/redux-intro/store.js
+++ src/redux-intro/store.js
@@ -24,19 +24,48 @@ function reducer(state = initialState, action){
 
 const store = createStore(reducer)
 
-store.dispatch({type: "account/deposit", payload: 1000 })
+// store.dispatch({type: "account/deposit", payload: 1000 })
 
+// console.log(store.getState());
+
+// store.dispatch({type: "account/withdraw", payload: 500 })
+
+// console.log(store.getState());
+
+// store.dispatch({type: "account/requestLoan", payload: {amount: 1000, purpose: "Buy a car"} })
+
+// console.log(store.getState());
+
+// store.dispatch({type: "account/payLoan", payload: {amount: 1000, purpose: "Buy a car"} })
+
+// console.log(store.getState());
+
+function deposit(amount){
+    return {type: "account/deposit", payload: amount }
+}
+
+function withdraw(amount){
+    return {type: "account/withdraw", payload: amount }
+}
+
+function requestLoan(amount, purpose){
+    return {type: "account/requestLoan", payload: {amount: amount, purpose: purpose} }
+}
+function payLoan(){
+    return {type: "account/payLoan"}
+}
+
+store.dispatch(deposit(1000))
+console.log(store.getState());
+store.dispatch(withdraw(500))
+console.log(store.getState());
+store.dispatch(requestLoan(5000, "Buy a car"))
+console.log(store.getState());
+store.dispatch(payLoan())
 console.log(store.getState());
 
-store.dispatch({type: "account/withdraw", payload: 500 })
 
-console.log(store.getState());
 
-store.dispatch({type: "account/requestLoan", payload: {amount: 1000, purpose: "Buy a car"} })
 
-console.log(store.getState());
 
-store.dispatch({type: "account/payLoan", payload: {amount: 1000, purpose: "Buy a car"} })
-
-console.log(store.getState());
 
